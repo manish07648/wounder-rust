da9ca8174614ea91002206a0ffea0187a54510f7
MANISH MANDAL <lmanishmandal111@gmail.com>
2026-04-16 14:56:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,28 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import connectDB from './config/db.js';
-import { PORT } from './config/utils.js';
+import { PORT, FRONTEND_URL } from './config/utils.js'; // Ab ye utils se hi uthayega
 import authRouter from './routes/auth.js';
 import postsRouter from './routes/posts.js';
 import { connectToRedis } from './services/redis.js';
+
 const app = express();
-const port = PORT || 5000;
+const port = PORT || 8080;
+
+// CORS Fix for Minikube
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(cookieParser());
 app.use(compression());
 
-// Connect to database
 connectDB();
-
-// Connect to redis
 connectToRedis();
 
-// API route
 app.use('/api/posts', postsRouter);
 app.use('/api/auth', authRouter);
 
