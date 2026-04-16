ae3c108aed3c54eb82f9b1d358e66f0f72443df0
MANISH MANDAL <lmanishmandal111@gmail.com>
2026-04-16 14:56:01 +0530

    Update utils.js

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI;
 const REDIS_URL = process.env.REDIS_URL;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://192.168.49.2:31000";
 
-export { MONGODB_URI, PORT, REDIS_URL };
+export { MONGODB_URI, PORT, REDIS_URL, FRONTEND_URL };
